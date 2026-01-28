2aa65aa61b2a7b5574023de1125608326bfb9f12
saitsk35 <saitsk35@gmail.com>
2026-01-28 18:12:01 +0530

    variables

--- catalogue.sh
+++ catalogue.sh
@@ -3,5 +3,5 @@ source common.sh
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 component=catalogue
 nodejs_setup
-dnf install mongodb-mongosh -y
-mongosh --host mongodb-dev.saikrishna.shop </app/db/master-data.js
+dnf install mongodb-mongosh -y &>>/tmp/roboshop.log
+mongosh --host mongodb-dev.saikrishna.shop </app/db/master-data.js &>>/tmp/roboshop.log
