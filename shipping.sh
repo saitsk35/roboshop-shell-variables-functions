57eb5dbb3ca3593ede102d17245bcd094c066358
saitsk35 <saitsk35@gmail.com>
2026-01-28 16:55:58 +0530

    variables

--- shipping.sh
+++ shipping.sh
@@ -2,7 +2,3 @@ source common.sh
 component=shipping
 java_setup
 
-mysql -h mysql-dev.saikrishna.shop -uroot -pRoboShop@1 < /app/db/schema.sql
-mysql -h mysql-dev.saikrishna.shop -uroot -pRoboShop@1 < /app/db/app-user.sql
-mysql -h mysql-dev.saikrishna.shop -uroot -pRoboShop@1 < /app/db/master-data.sql
-
