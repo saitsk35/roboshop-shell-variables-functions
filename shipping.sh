cddb918fe262625a0adaf97355886aff2fdc9004
saitsk35 <saitsk35@gmail.com>
2026-02-03 16:35:57 +0530

    variables

--- shipping.sh
+++ shipping.sh
@@ -4,8 +4,6 @@ java_setup
 
 dnf install mysql -y
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/schema.sql
-
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/app-user.sql
-
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/master-data.sql
+for sqlfile in schema app-user master-data;do
+  mysql -h mysql-dev.saikrishna.shop -uroot -pRoboShop@1 < /app/db/${sqlfile}.sql
+done
