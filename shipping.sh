649c4234deede98271b7919784a8c26db0e66699
saitsk35 <saitsk35@gmail.com>
2026-02-03 16:29:26 +0530

    variables

--- shipping.sh
+++ shipping.sh
@@ -2,3 +2,10 @@ source common.sh
 component=shipping
 java_setup
 
+dnf install mysql -y
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/schema.sql
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/app-user.sql
+
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/master-data.sql
